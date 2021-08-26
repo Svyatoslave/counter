17c0c9d2f1431c9ec012ead4cdf3b7e1d12ba9a6
Svyatoslav <svyatoslave07@gmail.com>
2021-08-26 11:39:32 +0300

    add redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import plusLogo from "../assets/icons/plus.png";
 import minusLogo from "../assets/icons/minus.png";
 import resetLogo from "../assets/icons/reset.png";
+import { decrement, increment, reset } from "../store/action/actionCreater";
 
-const Counter = () => (
+const Counter = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
+  const inc = () => {
+    dispatch(increment());
+  };
+
+  const dec = () => {
+    dispatch(decrement());
+  };
+  const res = () => {
+    dispatch(reset());
+  };
+
+  return (
     <div className="wrapper">
-        <div className="count">0</div>
-        <div className="wrapper__btn">
-            <button className="inc btn">
-                <img src={plusLogo} alt="plus"/>
-            </button>
-            <button className="dec btn">
-                <img src={minusLogo} alt="minus"/>
-            </button>
-            <button className="reset btn">
-                <img src={resetLogo} alt="reset"/>
-            </button>
-        </div>
+      <div className="count">{count}</div>
+      <div className="wrapper__btn">
+        <button onClick={inc} className="inc btn">
+          <img src={plusLogo} alt="plus" />
+        </button>
+        <button onClick={dec} className="dec btn">
+          <img src={minusLogo} alt="minus" />
+        </button>
+        <button onClick={res} className="reset btn">
+          <img src={resetLogo} alt="reset" />
+        </button>
+      </div>
     </div>
-)
+  );
+};
 
 export default Counter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store/store";
 
-import App from './App'
-
-import './styles/normalize.css';
+import "./styles/normalize.css";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
